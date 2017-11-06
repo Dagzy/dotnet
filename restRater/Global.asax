b22624ffd7f64fe1b42816591bc3d9d5ad397f71
Dagzy <quincye.williams@gmail.com>
2017-11-06 09:34:05 -0500

    Add project files.

---
+++ restRater/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="restRater.MvcApplication" Language="C#" %>
---
+++ restRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(restRater.Startup))]
+namespace restRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
